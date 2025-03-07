c727d32f02567c6caf66e54e6a68d42a654f6ffd
Felix Mölder <F.moelder@t-online.de>
2025-03-07 14:22:44 +0100

    HW leakage with Noise

--- example.py
+++ example.py
@@ -1,19 +1,16 @@
 import random
 import numpy as np
 from hint_solver import PyBP, PyGreedy
-from scipy.special import erf
 from scipy.stats import norm
 
 
 NVAR = 300
-NFAC = 2000
+NFAC = 500
 ETA = 2
 ETA_2 = 3
 N_GR = 10
 N_BP = 100
 STD_DEV = 1
-VALRAN = ETA * ETA_2 * NVAR
-
 
 def sample_bino(eta):
     r = sum([random.randint(0, 1) for _ in range(eta)]) - sum([random.randint(0, 1) for _ in range(eta)])
@@ -41,53 +38,21 @@ def twoComp(num):
     else:
         return num % (1 << 28)
 
-  
-#in: (list of) inner products(rhs) out: (list of) distributions (rhs_eq)
-#returns the distribution 
-def measure_HW_NoiseFree(inner_product):
-    hw = twoComp(inner_product).bit_count()
-    return [(hw, 1.0)]
-
 
 def measure_HW_Noise(inner_product):
     hw = twoComp(inner_product).bit_count()
     #Gaussian error:
     gau_err = np.random.normal(0,STD_DEV)
-    
     new_mu = hw + gau_err
     #Create a probability mass function from this mu
     x_vals = np.arange(29)
     pdf =  norm.pdf(x_vals,new_mu, STD_DEV)
-    #print(pdf)
     rounded_pdf = np.round(pdf, decimals=5)
-    indices = np.nonzero(rounded_pdf)[0]
-    res = [(i, rounded_pdf[i]) for i in indices] 
-    conv_res = [(a.tolist(),b.tolist()) for a,b in res]   
-    #print(conv_res)
     return rounded_pdf
 
-def measure_LSB(inner_products):
-    return [i & 1 for i in inner_products]
-    
 
-
-def measure_MSB(inner_product):
-    if inner_product >= 0:
-        msb = 0
-    else:
-        msb = 1
-    return msb
-
-
-#in: distribution out: distribution hints
-#Takes a list of distributions and creates the final list
-def attack(distribution):
-    #building hints..
-    return 0
-
-def attackLSB(distr):
-    return [[(x, 1.0) for x in range(-VALRAN,VALRAN) if (x & 1) == distri] for distri in distr]
-
+def attackHW(rhs,val):
+    return [[(x,measure_HW_Noise(rhi)[twoComp(x).bit_count()]) for x in range(-val,val)] for rhi in rhs]
 
 
 def main():
@@ -98,10 +63,26 @@ def main():
     print("Computing rhs..")
     rhs = [inner(ineq, s) for ineq in eqs]
     print("Setting distributions to actual values..")
-    #HW leakage model:
-    rhs_eq=[[(x,measure_HW_Noise(rhi)[twoComp(x).bit_count()]) for x in range(-VALRAN,VALRAN)] for rhi in rhs]
     
-    #print(len(rhs_eq))
+    # Estimate the maximum range of possible values v
+    li = []
+    for x in eqs:
+        sume = 0
+        for t in x:
+            sume += abs(t)
+        li.append(sume)
+    val = max(li)
+    
+    # Compute the distributions
+    list_of_dist_hints = attackHW(rhs, val) 
+    #rhs_eq2=[[(x,measure_HW_Noise(rhi).get(twoComp(x).bit_count(),0.0)) for x in range(-val,val)] for rhi in rhs]
+    
+    # Remove entries with probability 0.0
+    rhs_eq = []
+    for q in list_of_dist_hints:
+        g =[(s,p) for (s,p) in q if p != 0]
+        rhs_eq.append(g)
+    
     print("Creating BP and Greedy..")
     bp = PyBP(eqs, rhs_eq)
     greedy = PyGreedy(eqs, rhs_eq)
@@ -137,7 +118,7 @@ def main():
         k //= 2
         if k < NFAC//5:
             k = NFAC
-        print(f"{i}/{N_BP} (dist={dist:.1f}, k={k})", end='\r')
+        print(f"{i}/{N_BP} (dist={dist:.1f}, k={k})", end='\n')
     print()
 
 
